e9f7727f734f7b3dce2c01f521d6d3364986c44f
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-08-16 08:33:30 -0600

    Finished

--- src/api/src/test/java/com/observepoint/test/test/controllers/UserControllerUnitTestNoDB.java
+++ src/api/src/test/java/com/observepoint/test/test/controllers/UserControllerUnitTestNoDB.java
@@ -7,7 +7,6 @@ import com.observepoint.test.test.services.UserService;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.After;
 import org.junit.Before;
-//import org.junit.jupiter.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -16,9 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
-import org.springframework.test.context.ContextConfiguration;
+
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
